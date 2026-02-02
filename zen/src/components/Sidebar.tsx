6d1b8826af0735708f81a6e325ed5c096fb03108
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2026-02-02 17:54:50 +0000

    commit

--- zen/src/components/Sidebar.tsx
+++ zen/src/components/Sidebar.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { SunMoon } from "lucide-react";
 
 function Sidebar() {
+
+  {/* 
+    we need to make a light/dark icon
+    */}
   return (
     <>
-      <div className="text-3xl h-screen bg-side-bg max-w-60">
-        <h2 className=" font-roboto text-center">Welcome user!</h2>
+      <div className="pt-4 text-3xl h-screen bg-side-bg max-w-60 flex flex-col items-center">
+        <h2 className="font-roboto text-center">Welcome user!</h2>
+
+        <button className="mt-10 p-2 rounded-full hover:bg-gray-400 transition-colors"><SunMoon className="w-10 h-10"/></button>
+        
       </div>
     </>
   );
