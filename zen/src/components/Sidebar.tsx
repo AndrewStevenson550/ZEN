b4afc27a934572d10fd27c1e4291c4c5e501a0ee
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2026-02-03 14:31:42 +0000

    added dark mode and current date

--- zen/src/components/Sidebar.tsx
+++ zen/src/components/Sidebar.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { SunMoon } from "lucide-react";
 
 function Sidebar() {
-
-  {/* 
+  const currentDate = new Date();
+  {
+    /* 
     we need to make a light/dark icon
-    */}
+    */
+  }
   return (
     <>
-      <div className="pt-4 text-3xl h-screen bg-side-bg max-w-60 flex flex-col items-center">
-        <h2 className="font-roboto text-center">Welcome user!</h2>
-
-        <button className="mt-10 p-2 rounded-full hover:bg-gray-400 transition-colors"><SunMoon className="w-10 h-10"/></button>
-        
+      <div className="pt-4 text-3xl h-screen bg-side-bg max-w-60 flex flex-col items-center font-roboto">
+        <h2 className="text-center text-side-text">Welcome user!</h2>
+        <h5 className="pt-15 flex text-center text-side-text">Today is,</h5>
+        <h5 className="flex text-center text-side-text">
+          {currentDate.toDateString()}
+        </h5>
       </div>
     </>
   );
